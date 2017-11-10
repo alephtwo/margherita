747134c3cccff94288532e944aae10f41b164573
Ben Christianson <thhuntertgm@gmail.com>
2017-11-10 15:30:02 -0600

    Webpack with inferno

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.scss'
+
+import Inferno from 'inferno'
+import Thing from './components/hello.jsx'
+
+Inferno.render(<Thing />, document.getElementById('app'))
---
+++ webpack.config.babel.js
@@ -0,0 +1,33 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+
+const extractTextPlugin = new ExtractTextPlugin({
+  filename: '../css/app.css',
+  allChunks: true
+})
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, 'src', 'index.html'),
+  filename: '../index.html'
+})
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public', 'js')
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: { presets: ['env'], plugins: ['inferno'] }
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+    }]
+  },
+  plugins: [extractTextPlugin, htmlPlugin]
+}
