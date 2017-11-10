31923ca15d0a8312ef727a2aa26c721d4c4e88f4
Ben Christianson <thhuntertgm@gmail.com>
2017-11-10 15:52:16 -0600

    Added code climate

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -3,6 +3,6 @@ import Component from 'inferno-component'
 
 export default class Thing extends Component {
   render () {
-    return <h1>Hello, World!</h1>
+    return (<h1>Hello, World!</h1>);
   }
 }
