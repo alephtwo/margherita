af962e0d7be852ce9cb2cc037c1f6358bb023886
Ben Christianson <thhuntertgm@gmail.com>
2017-11-14 23:30:38 -0600

    Default values to empty string

--- src/reducers/calculators.js
+++ src/reducers/calculators.js
@@ -1,6 +1,6 @@
 import { List } from 'immutable'
 
-const reducer = (state = List.of({ price: 0, size: 0 }), action) => {
+const reducer = (state = List.of({ price: '', size: '' }), action) => {
   switch (action.type) {
     case 'ADD_ROW': return addRow(state)
     case 'REMOVE_ROW': return removeAllButLast(state)
@@ -9,7 +9,7 @@ const reducer = (state = List.of({ price: 0, size: 0 }), action) => {
   }
 }
 
-const addRow = (state) => state.push({ price: 0, size: 0 })
+const addRow = (state) => state.push({ price: '', size: '' })
 
 const removeAllButLast = (state) => {
   if (state.size > 1) {
