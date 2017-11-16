5ebab7072397c5bf4a664428b7e8a22b35d44a1f
Ben Christianson <thhuntertgm@gmail.com>
2017-11-15 21:28:40 -0600

    Fixed codeclimate issues

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -24,10 +24,10 @@ export default {
       query: { presets: ['env', 'react'] }},
     {test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!sass-loader')},
     {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml'},
-    {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-    {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-    {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream"},
-    {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"}]
+    {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff'},
+    {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff'},
+    {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/octet-stream'},
+    {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'}]
   },
   resolve: { extensions: ['.js', '.jsx'] },
   plugins: [extractTextPlugin, htmlPlugin]
